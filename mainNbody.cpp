631f9202d654a99d9371e04294babd35e7491d52
Louis Lammertyn <Louis.lammertyn6@gmail.com>
2022-12-18 17:04:23 +0100

    main met comments

--- mainNbody.cpp
+++ mainNbody.cpp
@@ -14,25 +14,33 @@ using namespace std;
 
 int main(){
     string file = "Initial_cond.txt";
-    nbody sim = init_sim(file);
-    double h = 1e-5;
-    double time = 0.01;
-    bool adapt = false;
-    //see func_nbody.hpp for the source-code of the integrators
+    nbody sim = init_sim(file);         //reads "Initial_cond.txt" and initializes sim accordingly (see func_nbody.hpp)
+                                        //for class nbody see class_nbody.hpp
+                                        
+    double h = 1e-4;                    //timestep
+    double time = 0.1;                  //integration time
 
-    //RK4integrator(h, time, sim, adapt, 1., 2.);
+    bool adapt = false;     //=true if you want an adaptive time step scheme
+
+    //see func_nbody.hpp for the source-code of the integrators
+    //remove the slashes of the integrators you want to test:
+
+    //RK4integrator(h, time, sim, adapt, 1., 2.5);      //last 2 numbers are parameters of the adaptive scheme (see func_nbody line 89)
     //verlet(h, time, sim);
     //FR(h, time, sim);
     //AM(h, time, sim);
     //AB(h, time, sim, 2);
 
-    bool hlooping = true;
-    bool vary_adapt_looping = false;
+
+    bool hlooping = false;          //=true if you want to generate output for comparing the rel. E. error for different h
+    bool vary_adapt_looping = false; //=true if you want to generate output for comparing the rel. E. error for different parameters...
+                                     //... of the adaptive time step scheme
 
     if (hlooping){
-        hloop(h, time, sim);    //see bottom of int_nbody.hpp
+        hloop(h, time, sim);    //see loops_nbody.hpp
     };
     if (vary_adapt_looping){
-        adapt_loop(h, time, 1., sim);   //see bottom of int_nbody.hpp
+        adapt_loop(h, time, 1., sim);   //see loops_nbody.hpp
     };
+    
     }
